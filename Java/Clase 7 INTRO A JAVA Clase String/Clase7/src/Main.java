4cf78ca1c593dc199dd8a4bde7f2db9a3c3c4ccf
Carlos Vargas <carlosdorianv@gmail.com>
2024-10-15 08:14:51 -0500

    Clase 7 termine colocando un switch para revisar la actividad que el usuario prefiera cuando lo prefiera (FALTA DOCUMENTACION)

--- Java/Clase 7 INTRO A JAVA Clase String/Clase7/src/Main.java
+++ Java/Clase 7 INTRO A JAVA Clase String/Clase7/src/Main.java
@@ -2,87 +2,115 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
     try{
         Scanner scanner = new Scanner(System.in);
-        //Actividad 1
-        Actividad1 actividad1 = new Actividad1();
-        try {
-            actividad1.procesarActividad1(scanner);
-        } catch (Exception e) {
-            System.out.println("Ocurrió un error inesperado actividad 1: " + e.getMessage());
-            System.exit(1);
-        }
+        System.out.println("Ingrese la actividad que desea ejecutar");
+        int x = scanner.nextInt();
+        switch(x){
+            case 1:
+                //Actividad 1
+                Actividad1 actividad1 = new Actividad1();
+                scanner = new Scanner(System.in);
+                try {
+                    actividad1.procesarActividad1(scanner);
+                } catch (Exception e) {
+                    System.out.println("Ocurrió un error inesperado actividad 1: " + e.getMessage());
+                    System.exit(1);
+                }
+                break;
+            case 2:
+                //Actividad 2
+                Actividad2 actividad2 = new Actividad2();
+                scanner = new Scanner(System.in);
+                try {
+                    actividad2.procesarActividad2(scanner);
+                } catch (RuntimeException e) {
+                    System.out.println("Ocurrio un error inesperado actividad 2: " + e.getMessage());
+                    System.exit(1);
+                }
+                break;
+            case 3:
+                //Actividad 3
+                Actividad3 actividad3 = new Actividad3();
+                scanner = new Scanner(System.in);
+                try {
+                    actividad3.procesarActividad3(scanner);
+                } catch (RuntimeException e) {
+                    System.out.println("Ocurrio un error inesperado actividad 3: " + e.getMessage());
+                    System.exit(1);
+                }
+                break;
+            case 4:
+                //Actividad 4
+                Actividad4 actividad4 = new Actividad4();
+                scanner = new Scanner(System.in);
+                try{
+                    actividad4.procesarActividad4(scanner);
+                }catch (RuntimeException e){
+                    System.out.println("Ocurrio un error inesperado actividad 4:" + e.getMessage());
+                    System.exit(1);
+                }
+                break;
+            case 5:
+                //Actividad 5
+                Actividad5 actividad5 = new Actividad5();
+                scanner = new Scanner(System.in);
+                try{
+                    actividad5.procesarActividad5(scanner);
+                }catch(RuntimeException e){
+                    System.out.println("Ocurrio un error inesperado actividad 5: " + e.getMessage());
+                }
+                break;
+            case 6:
+                //Actividad 6
+                Actividad6 actividad6 = new Actividad6();
+                scanner = new Scanner(System.in);
+                try{
+                    actividad6.procesarActividad6(scanner);
+                }catch(RuntimeException e){
+                    System.out.println("Ocurrio un error inesperado actividad 6: "+ e.getMessage());
+                }
+                break;
+            case 7:
+                //Actividad 7
+                Actividad7 actividad7 = new Actividad7();
+                scanner = new Scanner(System.in);
+                try{
+                    actividad7.procesarActividad7(scanner);
+                }catch(RuntimeException e){
+                    System.out.println("Ocurrio un error inesperado actividad 7: "+ e.getMessage());
+                }
+                break;
+            case 8:
+                //Actividad 8
+                Actividad8 actividad8 = new Actividad8();
+                scanner = new Scanner(System.in);
+                try{
+                    actividad8.procesarActividad8(scanner);
+                }catch(RuntimeException e){
+                    System.out.println("Ocurrio un error inesperado actividad 8: "+ e.getMessage());
+                }
+                break;
+            case 9:
+                //Actividad 9
+                Actividad9 actividad9 = new Actividad9();
+                scanner = new Scanner(System.in);
+                try{
+                    actividad9.procesarActividad9(scanner);
+                }catch(RuntimeException e){
+                    System.out.println("Ocurrio un error inesperado actividad 9: "+ e.getMessage());
+                }
+                break;
+            default:
+                System.out.println("Ingrese el numero correcto");
 
-        //Actividad 2
-        Actividad2 actividad2 = new Actividad2();
-        try {
-            actividad2.procesarActividad2(scanner);
-        } catch (RuntimeException e) {
-            System.out.println("Ocurrio un error inesperado actividad 2: " + e.getMessage());
-            System.exit(1);
         }
-        //Actividad 3
-        Actividad3 actividad3 = new Actividad3();
-        try {
-            actividad3.procesarActividad3(scanner);
-        } catch (RuntimeException e) {
-            System.out.println("Ocurrio un error inesperado actividad 3: " + e.getMessage());
-            System.exit(1);
-        }
-        //Actividad 4
-        Actividad4 actividad4 = new Actividad4();
-        try{
-            actividad4.procesarActividad4(scanner);
-        }catch (RuntimeException e){
-            System.out.println("Ocurrio un error inesperado actividad 4:" + e.getMessage());
-            System.exit(1);
-        }
-
-        //Actividad 5
-        Actividad5 actividad5 = new Actividad5();
-        try{
-            actividad5.procesarActividad5(scanner);
-        }catch(RuntimeException e){
-            System.out.println("Ocurrio un error inesperado actividad 5: " + e.getMessage());
-        }
-
-        //Actividad 6
-        Actividad6 actividad6 = new Actividad6();
-        try{
-            actividad6.procesarActividad6(scanner);
-        }catch(RuntimeException e){
-            System.out.println("Ocurrio un error inesperado actividad 6: "+ e.getMessage());
-        }
-
-        //Actividad 7
-        Actividad7 actividad7 = new Actividad7();
-        try{
-            actividad7.procesarActividad7(scanner);
-        }catch(RuntimeException e){
-            System.out.println("Ocurrio un error inesperado actividad 7: "+ e.getMessage());
-        }
-
-        //Actividad 8
-        Actividad8 actividad8 = new Actividad8();
-        try{
-            actividad8.procesarActividad8(scanner);
-        }catch(RuntimeException e){
-            System.out.println("Ocurrio un error inesperado actividad 8: "+ e.getMessage());
-        }
-        //Actividad 9
-        Actividad9 actividad9 = new Actividad9();
-        try{
-            actividad9.procesarActividad9(scanner);
-        }catch(RuntimeException e){
-            System.out.println("Ocurrio un error inesperado actividad 9: "+ e.getMessage());
-        }
-
         scanner.close();
-        }catch (Exception e){
-            System.out.println("Hay problemas con toda la Clase 7");
-            System.exit(1);
-            }
-
+    }catch (Exception e){
+        System.out.println("Hay problemas con toda la Clase 7");
+        System.exit(1);
+    }
 
     }
 }
